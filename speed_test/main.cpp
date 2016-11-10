b67204a633e2866e1fff9d0ec64b244936c73f5e
ivan <iivan1986@gmail.com>
2016-11-09 18:54:27 -0600

    test works

--- speed_test/main.cpp
+++ speed_test/main.cpp
@@ -9,25 +9,17 @@
 
 int main(int argc, char *argv[])
 {
-    std::atomic<bool> stop{false};
-    Prime::Test([](const Prime::BigNum& num){return Prime::IsPrimeAKS(num);}, "AKS", stop);
-
     TestBigNum();
 
-    Prime::ResourcesInfo info;
-    bool a = Prime::TestResources([](){return Prime::IsPrimeAKS(Prime::BigNum(17));}, info);
+    std::atomic<bool> stop{false};
+    std::thread thread([&stop](){
+        Prime::Test([](const Prime::BigNum& num){return Prime::IsPrimeAKS(num);}, "AKS", stop);}
+    );
 
-    const unsigned num = 100;
-
-    Prime::SoE soe = Prime::SoE(num);
-    for (int i = 1; i < num; i++)
-    {
-        bool aksR = IsPrimeAKS(Prime::BigNum(i));
-        bool soeR = soe.IsPrime(i);
-
-        std::cout << i << " " << aksR << ' ' << soeR << ' ' << (aksR == soeR ? 'V' : 'X') << std::endl;
-
-    }
+    std::cin.ignore();
+    stop = true;
+    if (thread.joinable())
+        thread.join();
 
     return 0;
 }
